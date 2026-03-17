4041fd0a1959cadfa0272476ec1f59a11244a9a4
EnzoLFranco <fenzolemos@gmail.com>
2026-03-17 20:52:50 -0300

    refactor(8): Clean up comments in init.sql for clarity and consistency

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -1,8 +1,6 @@
--- Cria um schema específico para a sua aplicação
 CREATE SCHEMA IF NOT EXISTS schema_api;
--- Define que o usuário padrão tenha acesso total a esse schema
+-- Aqui usamos o mesmo nome de usuário que vai estar no .env
 GRANT ALL PRIVILEGES ON SCHEMA schema_api TO api_user;
--- Define o schema padrão para pesquisas
 ALTER ROLE api_user
 SET search_path TO schema_api,
     public;
