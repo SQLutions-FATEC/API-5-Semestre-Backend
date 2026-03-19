7548b5400d83825be714324fba84bb4382bbc9cf
EnzoLFranco <fenzolemos@gmail.com>
2026-03-19 18:48:38 -0300

    fix(8): Update database privileges and role in init.sql for consistency

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS schema_api;
 -- Aqui usamos o mesmo nome de usuário que vai estar no .env
-GRANT ALL PRIVILEGES ON SCHEMA schema_api TO api_user;
-ALTER ROLE api_user
+GRANT ALL PRIVILEGES ON SCHEMA schema_api TO sqlutions;
+ALTER ROLE sqlutions
 SET search_path TO schema_api,
     public;
