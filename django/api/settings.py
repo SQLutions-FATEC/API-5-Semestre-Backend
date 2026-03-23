47da710db97a4fb7cdad825a6cffc7c303888dae
gabrielvascf <gabrielvasconcelos2709@gmail.com>
2026-03-23 20:17:10 -0300

    fix(21): disable debug and get secret key from env
    Also updates .env.example to include DJANGO_SECRET_KEY

--- django/api/settings.py
+++ django/api/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xh6juleh9olqvvq34or9f9r#s3@x*08zfylj_vuoo-w5_p=tbu'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
