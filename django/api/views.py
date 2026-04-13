37a1856fcc87409a619f80e157b167221edf9b32
GloBrito <gloriafbrito@gmail.com>
2026-04-12 21:33:30 -0300

    chore(49): sync views.py with develop-2

--- django/api/views.py
+++ django/api/views.py
@@ -161,7 +161,7 @@ def projeto_tarefas_timesheet_api(request, codigo_projeto):
                 for item in evolucao_qs
         }
 
-        response_data = {
+        Data = {
                         'projeto': {
                                 'codigo': projeto.codigo_projeto,
                                 'nome': projeto.nome_projeto,
@@ -170,8 +170,8 @@ def projeto_tarefas_timesheet_api(request, codigo_projeto):
                         'evolucao_horas': evolucao_horas,
                 }
         
-        return JsonResponse(response_data)
-git statu
+        return JsonResponse(Data)
+
 
 @require_GET
 def projeto_alertas_api(request, codigo_projeto):
