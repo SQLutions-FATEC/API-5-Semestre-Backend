cfaaa267d14741f9d1a17103ab001a1e76b761a6
EnzoLFranco <fenzolemos@gmail.com>
2026-04-14 13:43:00 -0300

    feat(61): implement request_analytics_api view for project analytics

--- django/api/views/requestAnalytics.py
+++ django/api/views/requestAnalytics.py
@@ -5,3 +5,6 @@ from django.views.decorators.http import require_GET
 from api.models import DimSolicitacao
 from .utils import obter_projeto, _dim_data_para_date
 
+@require_GET
+def request_analytics_api(request, codigo_projeto):
+    projeto = get_object_or_404(codigo_projeto=codigo_projeto)
