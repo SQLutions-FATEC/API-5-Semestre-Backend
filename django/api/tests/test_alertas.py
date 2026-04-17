10e05f53075e3756ba3774f6f8d9e6b73838c55b
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2026-04-16 21:30:56 -0300

    test(56): Fix tests for critical alerts endpoint in "pendente" case

--- django/api/tests/test_alertas.py
+++ django/api/tests/test_alertas.py
@@ -114,9 +114,8 @@ class ProjetoAlertasViewTest(TestCase):
         self.assertEqual(len(alertas['pedidos_atrasados']), 1)
         self.assertEqual(alertas['pedidos_atrasados'][0]['numero_pedido'], 'PED20')
 
-        self.assertEqual(len(alertas['pedidos_prioritarios_pendentes']), 2)
-        self.assertEqual(alertas['pedidos_prioritarios_pendentes'][0]['numero_pedido'], 'PED21')
-        self.assertEqual(alertas['pedidos_prioritarios_pendentes'][1]['numero_pedido'], 'PED24')
+        self.assertEqual(len(alertas['pedidos_prioritarios_pendentes']), 1)
+        self.assertEqual(alertas['pedidos_prioritarios_pendentes'][0]['numero_pedido'], 'PED24')
 
         self.assertEqual(len(alertas['materiais_obsoletos']), 1)
         self.assertTrue(alertas['materiais_obsoletos'][0]['vinculado_ao_projeto'])
