bf736cda511f71ca6a5254474b9075c1abbb8328
Cainã Melo <caina.melo@fatec.sp.gov.br>
2026-05-28 22:47:47 -0300

    refactor(102): enhance test setup by using dynamic program ID and improving assertions in CSV loader tests

--- django/etl/tests/test_csv_loader.py
+++ django/etl/tests/test_csv_loader.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 from django.test import TestCase
 
 from api.models import (
@@ -18,22 +17,24 @@ class CsvLoaderTest(TestCase):
 
     def setUp(self):
 
+        # Datas necessárias como DimData (FK do modelo)
         self.data_inicio = DimData.objects.create(
             dia=1,
             mes=1,
             ano=2025
         )
 
-        DimData.objects.create(
-            dia=1,
-            mes=1,
+        self.data_fim_prevista = DimData.objects.create(
+            dia=31,
+            mes=12,
             ano=2025
         )
 
+        # Programa com todas as FKs obrigatórias
         self.programa = DimPrograma.objects.create(
-            id=1,
             nome_programa="Programa Teste",
-            data_inicio=self.data_inicio
+            data_inicio=self.data_inicio,
+            data_fim_prevista=self.data_fim_prevista
         )
 
     def test_obter_ou_criar_data(self):
@@ -58,7 +59,7 @@ class CsvLoaderTest(TestCase):
                 "nome_projeto": "Projeto A",
                 "descricao": "Descricao A",
                 "status": "Ativo",
-                "programa_id": 1,
+                "programa_id": self.programa.id,
                 "data_inicio": "2025-01-01",
                 "data_fim": "2025-12-31"
             }
@@ -66,22 +67,12 @@ class CsvLoaderTest(TestCase):
 
         carregar_csv(df, "projetos")
 
-        self.assertEqual(
-            DimProjeto.objects.count(),
-            1
-        )
+        self.assertEqual(DimProjeto.objects.count(), 1)
 
         projeto = DimProjeto.objects.first()
 
-        self.assertEqual(
-            projeto.nome_projeto,
-            "Projeto A"
-        )
-
-        self.assertEqual(
-            projeto.programa.id,
-            1
-        )
+        self.assertEqual(projeto.nome_projeto, "Projeto A")
+        self.assertEqual(projeto.programa.id, self.programa.id)
 
     def test_carregar_csv_tipo_invalido(self):
 
@@ -89,7 +80,4 @@ class CsvLoaderTest(TestCase):
 
         carregar_csv(df, "tipo_invalido")
 
-        self.assertEqual(
-            DimProjeto.objects.count(),
-            0
-        )
+        self.assertEqual(DimProjeto.objects.count(), 0)
