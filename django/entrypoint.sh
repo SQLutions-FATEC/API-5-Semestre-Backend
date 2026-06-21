979a751c4c4ae42d564316f819af5dd00739a96f
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2026-06-21 12:45:26 -0300

    build(144): fix error message in entrypoint

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -10,14 +10,10 @@ if python manage.py smart_migrate; then
     echo "✅ SUCESSO: Migrations aplicadas perfeitamente."
     echo "=================================================="
 else
-    echo "❌ FATAL ERROR: Falha ao aplicar as migrations!"
-    echo "O container não pode subir com o banco desatualizado/quebrado."
+    echo "❌ FATAL ERROR: Falha ao aplicar as migrations!" >&2
+    echo "O container não pode subir com o banco desatualizado/quebrado." >&2
     
-    # Opcional: Se você usa Slack, Discord ou Teams, pode disparar um alerta aqui!
-    # curl -X POST -H 'Content-type: application/json' --data '{"text":"🚨 Erro de Migration na API!"}' SEU_WEBHOOK_URL
-    
-    echo "=================================================="
-    # Força o container a "morrer" informando código de erro para a AWS
+    echo "==================================================" >&2
     exit 1
 fi
 
